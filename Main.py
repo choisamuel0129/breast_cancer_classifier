5ca390940eb56cd7c0d40f6fd4b729f70df14fb6
Samuel <sksksjakskska@gmail.com>
2026-04-04 22:30:30 +0900

    feat(data): 유방암 데이터셋 분할 로직 추가
    데이터셋 입력 변수명을 inputs로 정리함
    데이터 shape 출력 코드를 추가함
    훈련, 검증, 테스트 데이터 분할 로직을 추가함
    사용하지 않는 import를 제거함

--- Main.py
+++ Main.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import keras
 
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
@@ -10,8 +8,11 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 #데이터 불러오기
 data = load_breast_cancer()
+print(data.data.shape)
 
-input = data.data
+inputs = data.data
 target = data.target
 
-
+#데이터 나누기
+train_input, test_input, train_target, test_target = train_test_split(inputs, target, stratify=target, test_size=0.2) #훈련데이터와 테스트+검증 데이터 분리
+train_input, val_input, train_target, val_target = train_test_split(train_input, train_target, stratify=train_target, test_size=0.2)#테스트데이터와 검증데이터 분리
